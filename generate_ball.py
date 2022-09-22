fa160557930fc4ef9f1db87355d8c96e1fed63f5
Albert-Miao <amiao541769@gmail.com>
2022-09-22 16:51:32 -0700

    Created generate_ball and track_ball files and wrote basic skeleton for generate_ball

---
+++ generate_ball.py
@@ -0,0 +1,39 @@
+import cv2
+import math
+import numpy as np
+import os
+
+import argparse
+
+
+class Ball:
+    def __init__(self, color, acceleration):
+        return
+
+    def nextFrame(self):
+        return tuple()
+
+
+class ScreenWriter:
+    def __init__(self, resolution, num_frames):
+        return
+
+    def generate_image(self):
+        return 0
+
+
+# TODO: creates argparse object and passes to parse_args function
+# TODO: creates Ball object to be run, initialized with core args
+# TODO: creates ScreenWriter object, initialized with core args
+# TODO: For however long the video lasts, grab the next frame from Ball with something like nextFrame().
+#       Probably returns info of position, color, major and minor axis.
+# TODO: Pass relevant info to ScreenWriter and append result to images list
+# TODO: Save images.
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ track_ball.py
@@ -0,0 +1,12 @@
+import cv2
+import math
+import numpy as np
+import os
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
