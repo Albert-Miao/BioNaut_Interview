6c5e8aeebf42c673a9c034f822d84976951858e9
Albert-Miao <amiao541769@gmail.com>
2022-09-22 18:08:48 -0700

    Finished init_test of Ball

--- tests/generate_ball_test.py
+++ tests/generate_ball_test.py
@@ -2,17 +2,22 @@ from generate_ball import *
 
 import unittest
 
+
 class TestBallMethods(unittest.TestCase):
     def test_init(self):
         ball = Ball((127, 100, 156), 9.81, 300, 5, True)
-        
+
         self.assertTrue(ball.color[0] == 127 and ball.color[1] == 100 and ball.color[2] == 156)
         self.assertTrue(ball.acceleration == 9.81)
         self.assertTrue(ball.height == 300)
         self.assertTrue(ball.count_bounces == True)
-        
+
         self.assertTrue(hasattr(ball, 'max_bounces'))
         self.assertTrue(hasattr(ball, 'curr_bounces'))
         self.assertTrue(not hasattr(ball, 'max_frames') and not hasattr(ball, 'curr_frames'))
         self.assertTrue(ball.max_bounces == 5)
-        
+
+        self.assertTrue(hasattr(ball, 'hor_vel'))
+        self.assertTrue(hasattr(ball, 'ver_vel'))
+        self.assertTrue(ball.hor_vel == PRE_SCALED_HOR_VEL)
+        self.assertTrue(ball.ver_vel == 0)
