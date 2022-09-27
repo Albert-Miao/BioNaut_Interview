2f5ed5ebe05af78213ac96716553e68724f102b8
Albert-Miao <amiao541769@gmail.com>
2022-09-27 16:10:29 -0700

    Wrote parse_args test

--- tests/track_ball_test.py
+++ tests/track_ball_test.py
@@ -1,3 +1,12 @@
 from track_ball import *
 
 import unittest
+
+
+class TestHelperMethods(unittest.TestCase):
+    def test_parse_args1(self):
+        args = parse_args([
+            '--path', '../sample_videos/1.avi'
+        ])
+
+        self.assertTrue(args['path'] == '../sample_videos/1.avi')
--- track_ball.py
+++ track_ball.py
@@ -14,24 +14,30 @@ def main():
     assert capture.isOpened() and "Error opening video. Could be a multitude of problems, but likely corruption."
 
     fps = capture.get(cv2.CAP_PROP_FPS)
-
     while capture.isOpened():
         ret, frame = capture.read()
         if not ret:
             break
 
+
+
         cv2.imshow('Frame', frame)
         cv2.waitKey(int(1000/fps))
+
+
+
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--path', dest='path', type=str, default='sample_videos/1.avi',
                         help='Path to video to load.')
-
     args = parser.parse_args(args)
-
     assert os.path.exists(args.path)
+
+    args = {
+        'path': args.path
+    }
 
     return args
 
